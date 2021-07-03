ff736874b58a8c392dfccb5c75ac1b5d648553ff
Andre Masson <amwebexpert@gmail.com>
2021-07-03 15:54:51 -0400

    feat: added demo of features

--- App.tsx
+++ App.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, Text} from 'react-native';
+import {Alert, Button, Platform, SafeAreaView, ScrollView, StyleSheet, Text, ToastAndroid} from 'react-native';
+import {getSecureData, getSupportedBiometryType, removeSecureData, storeSecureData} from './secure-service';
 
 const App = () => {
+  const toastMessage = (msg: string) => {
+    if (Platform.OS === 'android') {
+      ToastAndroid.show(msg, ToastAndroid.SHORT);
+    } else {
+      Alert.alert(msg);
+    }
+  };
+
+  const showSupportedBiometryType = async () => {
+    const result = await getSupportedBiometryType();
+    toastMessage(`Supported: ${result}`);
+  };
+
+  const storeDataDemo = async () => {
+    const user = {
+      username: 'zoemiller@mailinator.com',
+      password: 'test1111',
+      refreshToken: 'My-Refresh-Token',
+    };
+
+    const result = await storeSecureData(user);
+    toastMessage(`Storage result: ${result}`);
+  };
+
+  const removeDataDemo = async () => {
+    const result = await removeSecureData();
+    toastMessage(`Cleanup result: ${result}`);
+  };
+
+  async function getSecureDataDemo() {
+    try {
+      const secureData = await getSecureData();
+      toastMessage(`Secure data retrieved:\n ${JSON.stringify(secureData, null, 2)}`);
+    } catch (error) {
+      toastMessage("Keychain couldn't be accessed!");
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Text style={styles.title}>Test</Text>
+        <Button title="Supported biometric type" onPress={showSupportedBiometryType}>
+          Supported biometric type
+        </Button>
+        <Button title="Store secure data" onPress={storeDataDemo}>
+          Store secure data
+        </Button>
+        <Button title="Remove secure data" onPress={removeDataDemo}>
+          Remove secure data
+        </Button>
+        <Button title="Biometric test" onPress={getSecureDataDemo}>
+          Biometric test
+        </Button>
       </ScrollView>
     </SafeAreaView>
   );
---
+++ secure-service.ts
@@ -0,0 +1,59 @@
+import * as Keychain from 'react-native-keychain';
+
+const options: Keychain.Options = {
+  accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY_OR_DEVICE_PASSCODE,
+  accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED,
+  authenticationType: Keychain.AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
+  authenticationPrompt: {
+    title: 'Identification biométrique requise',
+    subtitle: 'Service sécurisé via la biométrie (empreintes ou identification faciale)',
+    description: 'Nous tentons de vous identifier de la manière la plus sécuritaire possible sur votre appareil',
+    cancel: 'Annuler la biométrie',
+  },
+};
+
+export enum BiometryTypeEnum {
+  FINGERPRINT = 'Fingerprint',
+  FACE = 'Face',
+  IRIS = 'Iris',
+}
+
+export const getSupportedBiometryType = async (): Promise<null | BiometryTypeEnum> => {
+  const type = await Keychain.getSupportedBiometryType(options);
+  // console.log('Supported biometric type', type);
+  switch (type) {
+    case Keychain.BIOMETRY_TYPE.FACE:
+    case Keychain.BIOMETRY_TYPE.FACE_ID:
+      return BiometryTypeEnum.FACE;
+
+    case Keychain.BIOMETRY_TYPE.TOUCH_ID:
+    case Keychain.BIOMETRY_TYPE.FINGERPRINT:
+      return BiometryTypeEnum.FINGERPRINT;
+
+    case Keychain.BIOMETRY_TYPE.IRIS:
+      return BiometryTypeEnum.IRIS;
+
+    default:
+      return null;
+  }
+};
+
+export const storeSecureData = async (data: unknown): Promise<boolean> => {
+  const result = await Keychain.setGenericPassword('secure-data', JSON.stringify(data), options);
+  console.log('Storage result', result);
+
+  return typeof result === 'object';
+};
+
+export const getSecureData = async (): Promise<any | null> => {
+  const credentials = await Keychain.getGenericPassword(options);
+  if (credentials) {
+    return JSON.parse(credentials.password);
+  } else {
+    return null;
+  }
+};
+
+export const removeSecureData = async (): Promise<boolean> => {
+  return Keychain.resetGenericPassword();
+};
